6c7c85651bedc9e0299d88d5233e9e3fa7ca9085
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-02 18:16:09 +0100

    Update edit.php

--- www/sacs/edit.php
+++ www/sacs/edit.php
@@ -69,5 +69,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <a href="index.php" class="btn btn-secondary">Annuler</a>
     </form>
 </div>
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1000, // Durée de l'animation (en ms)
+    });
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+</script>
 </body>
 </html>
