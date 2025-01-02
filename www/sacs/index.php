8d5e8f447315d31d996f532691888381a1403210
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-02 18:07:37 +0100

    Update index.php

--- www/sacs/index.php
+++ www/sacs/index.php
@@ -11,6 +11,9 @@ $sacs = $stmt->fetchAll();
 <head>
     <title>Liste des Sacs Médicaux</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"> <!-- Animate.css -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.4/dist/aos.css"> <!-- AOS -->
 <style>
     /* Style pour le menu */
     .navbar {
