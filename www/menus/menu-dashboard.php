5a94de4d96688bcf7cd38886fe0ac66008e12c95
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-02 17:07:23 +0100

    Update menu-dashboard.php

--- www/menus/menu-dashboard.php
+++ www/menus/menu-dashboard.php
@@ -1,25 +1,35 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Gestion Médicale</a>
+        <a class="navbar-brand fw-bold" href="#">Gestion Médicale</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php">Tableau de Bord</a>
+                    <a class="nav-link active" href="dashboard.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Accéder au tableau de bord">
+                        <i class="fas fa-home"></i> Tableau de Bord
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="sacs/index.php">Gestion des Sacs</a>
+                    <a class="nav-link" href="sacs.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Gérer les sacs">
+                        <i class="fas fa-briefcase-medical"></i> Gestion des Sacs
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="medicaments/index.php">Gestion des Médicaments</a>
+                    <a class="nav-link" href="medicaments.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Gérer les médicaments">
+                        <i class="fas fa-pills"></i> Gestion des Médicaments
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="users/manage_users.php">Gestion des Utilisateurs</a>
+                    <a class="nav-link" href="users.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Gérer les utilisateurs">
+                        <i class="fas fa-users"></i> Gestion des Utilisateurs
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Déconnexion</a>
+                    <a class="nav-link" href="logout.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Se déconnecter">
+                        <i class="fas fa-sign-out-alt"></i> Déconnexion
+                    </a>
                 </li>
             </ul>
         </div>
