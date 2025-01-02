fcf1fbc497fca95ad8e998c158727f27f091179d
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-02 18:07:06 +0100

    Update index.php

--- www/sacs/index.php
+++ www/sacs/index.php
@@ -69,5 +69,18 @@ $sacs = $stmt->fetchAll();
         </tbody>
     </table>
 </div>
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1000, // Durée de l'animation (en ms)
+    });
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+</script>
 </body>
 </html>
