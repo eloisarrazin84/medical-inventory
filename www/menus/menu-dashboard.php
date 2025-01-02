4614298362415dfcb161d408eebad62ac575d214
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-02 17:02:52 +0100

    Create menu-dashboard.php

---
+++ www/menus/menu-dashboard.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Gestion Médicale</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.php">Tableau de Bord</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sacs/index.php">Gestion des Sacs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="medicaments/index.php">Gestion des Médicaments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="users/manage_users.php">Gestion des Utilisateurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
