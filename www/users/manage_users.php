4a376d31be698679e1ecf581047a11c6d95f8a4f
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-03 15:38:23 +0100

    Update manage_users.php

--- www/users/manage_users.php
+++ www/users/manage_users.php
@@ -93,13 +93,12 @@ $users = $stmt->fetchAll();
         </tbody>
     </table>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
 <script>
-    AOS.init({
-        duration: 1000, // Durée de l'animation (en ms)
-    });
+    AOS.init({ duration: 1000 });
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
