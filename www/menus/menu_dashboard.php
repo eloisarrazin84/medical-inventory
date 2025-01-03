46f091b3419cfd891bd25618894edc2164a504fa
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-03 15:21:14 +0100

    Update menu_dashboard.php

--- www/menus/menu_dashboard.php
+++ www/menus/menu_dashboard.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Navbar Dropdown</title>
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
     <div class="container-fluid">
         <!-- Logo -->
@@ -47,15 +36,3 @@
         </div>
     </div>
 </nav>
-
-<!-- JavaScript Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-    // Initialisation des tooltips Bootstrap
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-</script>
-</body>
-</html>
