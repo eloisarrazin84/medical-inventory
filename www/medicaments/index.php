644b83d33f7490a901a4b26ebcab48a6678ed02b
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-07 10:16:18 +0100

    Update index.php

--- www/medicaments/index.php
+++ www/medicaments/index.php
@@ -66,7 +66,7 @@ $medicaments = $stmt->fetchAll();
 </head>
 <body>
 <!-- Inclure le menu -->
-<?php include '../menus/menu_medicaments.php'; ?>
+<?php include '../menus/menu_usersmanage.php'; ?>
 <div class="container mt-5">
     <h1>Gestion des Médicaments</h1>
     <a href="choisir_sac.php" class="btn btn-primary mb-3">Choisir un autre sac</a>
