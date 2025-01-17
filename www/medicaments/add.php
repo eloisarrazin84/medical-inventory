119916d2d33b5f9a4e67faa8fd709183de666bb0
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 18:51:08 +0100

    Update add.php

--- www/medicaments/add.php
+++ www/medicaments/add.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="fr">
 <head>
     <title>Ajouter un Médicament</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
