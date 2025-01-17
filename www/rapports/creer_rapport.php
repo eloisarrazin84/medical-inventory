b7e9719bf0c800ab752870cabc1c195a091aea49
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 18:19:08 +0100

    Update creer_rapport.php

--- www/rapports/creer_rapport.php
+++ www/rapports/creer_rapport.php
@@ -46,6 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Déclarer un Rapport</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="../css/styles.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
