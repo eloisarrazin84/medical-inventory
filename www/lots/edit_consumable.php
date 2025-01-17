6a5a176b005ec49dc5b858d126f4862d895afe40
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 18:16:20 +0100

    Update edit_consumable.php

--- www/lots/edit_consumable.php
+++ www/lots/edit_consumable.php
@@ -38,6 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <title>Modifier un Consommable</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="../css/styles.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
