808b6783f5ccfc045b40d1eaba94c7b69fb068ce
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 18:17:52 +0100

    Update signaler_incident.php

--- www/incidents/signaler_incident.php
+++ www/incidents/signaler_incident.php
@@ -78,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Signaler un Incident</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="../css/styles.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
