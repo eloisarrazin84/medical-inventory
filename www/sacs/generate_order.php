65c4b4f4532c549415e3cc092820d82db8153e25
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 14:05:44 +0100

    Update generate_order.php

--- www/sacs/generate_order.php
+++ www/sacs/generate_order.php
@@ -32,8 +32,26 @@ $stmt = $pdo->prepare("
 $stmt->execute([$sac_id]);
 $medicaments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if (empty($medicaments)) {
-    die("Aucun médicament expiré ou proche de l'expiration pour ce sac.");
+// Récupérer les lots associés au sac
+$stmt = $pdo->prepare("SELECT * FROM lots WHERE sac_id = ?");
+$stmt->execute([$sac_id]);
+$lots = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Récupérer les consommables expirés ou proches de l'expiration pour chaque lot
+$consommables = [];
+foreach ($lots as $lot) {
+    $stmt = $pdo->prepare("
+        SELECT consommables.nom, consommables.description, consommables.quantite, consommables.date_expiration, lots.nom AS lot_nom
+        FROM consommables
+        INNER JOIN lots ON consommables.lot_id = lots.id
+        WHERE lots.id = ? AND (consommables.date_expiration < CURDATE() OR consommables.date_expiration BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 30 DAY))
+    ");
+    $stmt->execute([$lot['id']]);
+    $consommables = array_merge($consommables, $stmt->fetchAll(PDO::FETCH_ASSOC));
+}
+
+if (empty($medicaments) && empty($consommables)) {
+    die("Aucun médicament ou consommable à commander pour ce sac.");
 }
 
 // Générer le PDF
@@ -47,7 +65,11 @@ $html = "
     <p><strong>Nom du Sac :</strong> {$sac['nom']}</p>
     <p><strong>Description :</strong> {$sac['description']}</p>
     <p><strong>Lieu de Stockage :</strong> {$sac['lieu_nom']}</p>
-    <hr>
+    <hr>";
+
+// Ajouter les médicaments à commander
+if (!empty($medicaments)) {
+    $html .= "
     <h2 style='text-align: center;'>Médicaments à Commander</h2>
     <table border='1' cellpadding='5' cellspacing='0' style='width: 100%; border-collapse: collapse;'>
         <thead>
@@ -60,23 +82,57 @@ $html = "
             </tr>
         </thead>
         <tbody>";
-foreach ($medicaments as $med) {
+    foreach ($medicaments as $med) {
+        $html .= "
+            <tr>
+                <td>{$med['nom']}</td>
+                <td>{$med['description']}</td>
+                <td>{$med['quantite']}</td>
+                <td>{$med['numero_lot']}</td>
+                <td>{$med['date_expiration']}</td>
+            </tr>";
+    }
     $html .= "
-        <tr>
-            <td>{$med['nom']}</td>
-            <td>{$med['description']}</td>
-            <td>{$med['quantite']}</td>
-            <td>{$med['numero_lot']}</td>
-            <td>{$med['date_expiration']}</td>
-        </tr>";
-}
-$html .= "
         </tbody>
-    </table>
+    </table>";
+}
+
+// Ajouter les consommables à commander
+if (!empty($consommables)) {
+    $html .= "
+    <h2 style='text-align: center;'>Consommables à Commander</h2>
+    <table border='1' cellpadding='5' cellspacing='0' style='width: 100%; border-collapse: collapse;'>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Quantité</th>
+                <th>Date d'Expiration</th>
+                <th>Lot Associé</th>
+            </tr>
+        </thead>
+        <tbody>";
+    foreach ($consommables as $cons) {
+        $html .= "
+            <tr>
+                <td>{$cons['nom']}</td>
+                <td>{$cons['description']}</td>
+                <td>{$cons['quantite']}</td>
+                <td>{$cons['date_expiration']}</td>
+                <td>{$cons['lot_nom']}</td>
+            </tr>";
+    }
+    $html .= "
+        </tbody>
+    </table>";
+}
+
+$html .= "
     <br><br>
     <p><strong>Visa :</strong> _______________________________</p>
 ";
 
+// Charger et rendre le PDF
 $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
