765ba41159e2c95b59a332bd6a4554ba70998c04
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-17 18:51:43 +0100

    Update edit_lot.php

--- www/lots/edit_lot.php
+++ www/lots/edit_lot.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="fr">
 <head>
     <title>Modifier un Lot</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
      <link href="../css/styles.css" rel="stylesheet">
 </head>
