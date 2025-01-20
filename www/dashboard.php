591b19aabd826fd8fa3b3ee0f9ebf59270e8fd00
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-01-20 09:02:20 +0100

    Update dashboard.php

--- www/dashboard.php
+++ www/dashboard.php
@@ -78,7 +78,7 @@ $total_rapports = $stmt->fetch()['total_rapports'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
 </head>
 <body>
 <?php include 'menus/menu_dashboard.php'; ?>
