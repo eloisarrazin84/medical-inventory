6c39ab485c27d1d2432bf27ef837d8646f11b939
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-03-06 09:47:19 +0100

    Update update_notifications.php

--- www/notifications/update_notifications.php
+++ www/notifications/update_notifications.php
@@ -1,7 +1,7 @@
 <?php
 include '../includes/db.php';
 
-$pdo->query("UPDATE notifications SET is_read = 1");
+$pdo->query("UPDATE notifications SET statuts = 1");
 
 echo json_encode(["success" => true]);
 ?>
